3b3c7271924f43c5842c980e04dd60694d4934fe
Ayush <ayushkurmi97@gmail.com>
2021-11-29 21:05:41 +0530

    localstorage added

--- src/Main.js
+++ src/Main.js
@@ -1,39 +1,54 @@
-import React, { Component } from "react";
-import CreateTask from "./CreateTask";
-import TaskList from "./TaskList";
-import "./Main.css";
+import React, { Component } from 'react'
+import CreateTask from './CreateTask'
+import TaskList from './TaskList'
+import './Main.css'
 export default class Main extends Component {
   constructor(props) {
-    super(props);
-    this.state = { tasks: [] };
-    this.createTask = this.createTask.bind(this);
-    this.deleteTask = this.deleteTask.bind(this);
-    this.editTask = this.editTask.bind(this);
-    this.toggleTask = this.toggleTask.bind(this);
+    super(props)
+    let allTasks = localStorage.getItem('tasks')
+    if (allTasks === null) {
+      allTasks = []
+    } else {
+      allTasks = JSON.parse(allTasks)
+    }
+    this.state = { tasks: allTasks }
+    this.createTask = this.createTask.bind(this)
+    this.deleteTask = this.deleteTask.bind(this)
+    this.editTask = this.editTask.bind(this)
+    this.toggleTask = this.toggleTask.bind(this)
   }
   createTask(task) {
-    if (task.trim() === "") {
-      alert("Task cannot be empty");
-      return;
+    if (task.trim() === '') {
+      alert('Task cannot be empty')
+      return
     }
-    let newTask = { taskName: task.trim(), isCompleted: false };
-    let allTasks = [...this.state.tasks, newTask];
-    this.setState({ tasks: allTasks });
+    let newTask = { taskName: task.trim(), isCompleted: false }
+    let allTasks = [...this.state.tasks, newTask]
+    this.setState({ tasks: allTasks }, () => {
+      localStorage.setItem('tasks', JSON.stringify(this.state.tasks))
+    })
   }
   deleteTask(id) {
-    let arr = this.state.tasks.filter((t, index) => index !== id);
-    this.setState({ tasks: arr });
+    let arr = this.state.tasks.filter((t, index) => index !== id)
+
+    this.setState({ tasks: arr }, () => {
+      localStorage.setItem('tasks', JSON.stringify(this.state.tasks))
+    })
   }
   editTask(index, value) {
-    console.log("editTask called");
-    let arr = [...this.state.tasks];
-    arr[index].taskName = value;
-    this.setState({ tasks: arr });
+    console.log('editTask called')
+    let arr = [...this.state.tasks]
+    arr[index].taskName = value
+    this.setState({ tasks: arr }, () => {
+      localStorage.setItem('tasks', JSON.stringify(this.state.tasks))
+    })
   }
   toggleTask(index) {
-    let arr = [...this.state.tasks];
-    arr[index].isCompleted = !arr[index].isCompleted;
-    this.setState({ tasks: arr });
+    let arr = [...this.state.tasks]
+    arr[index].isCompleted = !arr[index].isCompleted
+    this.setState({ tasks: arr }, () => {
+      localStorage.setItem('tasks', JSON.stringify(this.state.tasks))
+    })
   }
   render() {
     return (
@@ -50,6 +65,6 @@ export default class Main extends Component {
           />
         </div>
       </div>
-    );
+    )
   }
 }
